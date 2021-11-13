434e1ba5e7c07983b10c4e02f66f4cf46b09be99
MostafaKadry <mostafakadry806@gmail.com>
2021-11-13 23:32:03 +0200

    upload v.3 after fix classname issue

--- src/components/contactus.js
+++ src/components/contactus.js
@@ -2,23 +2,23 @@ import React from "react";
 
 const ContactUS = () => {
   return (
-    <div class="row">
+    <div className="row">
       {/* <!-- CONTENT FOR THE MOBILE NUMBER  --> */}
-      <div class="col-6 pt-1 d-block">
-        <div class="contact-box__info">
-          <a href="#" class="contact-box__info--title">
+      <div className="col-6 pt-1 d-block">
+        <div className="contact-box__info">
+          <a href="#" className="contact-box__info--title">
             01009158274
           </a>
           <p>تم برمجته بواسطة مصطفى قدري</p>
         </div>
       </div>
       {/* <!-- CONTENT FOR EMAIL  --> */}
-      <div class="col-6 pt-1 d-block">
-        <div class="contact-box__info">
-          <a href="#" class="contact-box__info--title">
+      <div className="col-6 pt-1 d-block">
+        <div className="contact-box__info">
+          <a href="#" className="contact-box__info--title">
             mostafakadry806@gmail.com
           </a>
-          <p class="contact-box__info--subtitle">للدعم الفني</p>
+          <p className="contact-box__info--subtitle">للدعم الفني</p>
         </div>
       </div>
     </div>
